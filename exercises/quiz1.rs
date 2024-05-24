a6b3866a2dcf9b2d311612f74e411e3f6c9c361e
Aaron Lloyd <aaron.ilift@gmail.com>
2024-05-24 17:27:27 +0100

    Quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: u16) -> u16 {
+    if num_apples <= 40 {
+        return 2 * num_apples;
+    }
+    num_apples
+}
 
 // Don't modify this function!
 #[test]
